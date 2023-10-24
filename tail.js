51a08056e415a152af38a3c2d203ba0f01a311f7
dylanhamada <dylan.hamada@gmail.com>
2023-10-24 08:20:51 -0400

    Refactor tail.js to move tests to new file and export as module

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-    return;
-  }
-
-  console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  return;
-};
-
 const tail = (arr) => {
   // create variable to house new array
   // make new variable equal to old array
@@ -18,10 +8,4 @@ const tail = (arr) => {
   return newArr;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(words)[0], words[1]);
-assertEqual(tail(words).length, words.length - 1);
-assertEqual(tail([1]), []);
+module.exports = tail;
